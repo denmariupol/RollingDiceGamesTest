34a9b7a98daa441a9f39b1373080dd86a475d78b
den <denmariupol@mail.ru>
2016-09-23 13:08:12 +0300

    final commit

--- RollingDiceGames/Program.cs
+++ RollingDiceGames/Program.cs
@@ -10,6 +10,24 @@ namespace RollingDiceGames
     {
         static void Main(string[] args)
         {
+            Numbers numbers = new Numbers();
+            List<List<int>> set = new List<List<int>>();
+            Random rand = new Random();
+            int k = rand.Next(1, 10);
+            Console.WriteLine("k = " + k);
+            for (int i = 0;i < k;i++)
+                set.Add(numbers.GenerateNumbers());
+            foreach (List<int> n in set)
+            {
+                Console.Write("Length=" + n.Count + "  Set=");
+                numbers.Check(n);
+                foreach(int num in n)
+                    Console.Write( num+" ");
+                Console.WriteLine("Count = " + numbers.Count);
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
         }
     }
 }
