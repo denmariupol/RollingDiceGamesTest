07fb0d5b1841c4a29d12c39fb77d2ea768d24ea2
den <denmariupol@mail.ru>
2016-09-23 17:43:18 +0300

    qq

--- RollingDiceGames/Program.cs
+++ RollingDiceGames/Program.cs
@@ -10,6 +10,16 @@ namespace RollingDiceGames
     {
         static void Main(string[] args)
         {
+            Random rand = new Random();
+            int[] numbers = new int[5] { rand.Next(1, 30), rand.Next(1, 30), rand.Next(1, 30), rand.Next(1, 30), rand.Next(1, 30)};
+            Fibonacci(1, 1, 1,numbers);
+            Console.ReadLine();
+        }
+        public static void Fibonacci(int a, int b, int count,params int[] num)
+        {
+            Console.WriteLine(a);
+            if (count < 30)
+                Fibonacci(b, a + b, count + 1,num);
         }
     }
 }
